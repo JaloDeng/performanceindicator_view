b440bbce50d40a287da36168bf28ebb6b49e0a85
JaloDeng <651379728@qq.com>
2019-08-01 16:01:28 +0800

    添加积分记录表界面

---
+++ src/components/EmployeeIntegral.vue
@@ -0,0 +1,229 @@
+<template>
+  <div>
+    <el-container>
+      <h2>积分记录表</h2>
+      <el-header>
+        <div>
+          参与者：<el-input clearable style="width: 200px;" size="mini" @keyup.enter.native="search" v-model="integrals"></el-input>
+          项目：<el-input clearable style="width: 200px;" size="mini" @keyup.enter.native="search" v-model="employees"></el-input>
+          获取时间：
+          <el-date-picker v-model="integralTime" type="datetimerange" range-separator="-" format="yyyy-MM-dd HH:mm:ss" value-format="yyyy-MM-dd HH:mm:ss" size="mini"
+            :start-placeholder="searchParams.integralStartTime" :end-placeholder="searchParams.integralEndTime" @change="integralSearchTimeChange">
+          </el-date-picker>
+          <el-button type="primary" size="mini" icon="el-icon-search" @click="search">搜索</el-button>
+          <el-button type="success" size="mini" icon="el-icon-plus" @click="showAddView()">添加</el-button>
+        </div>
+      </el-header>
+      <el-main>
+        <div>
+          <el-table :data="employeeIntegrals" v-loading="tableLoading" size="mini" border @sort-change="tableSortChange" :default-sort="{prop: 'integralTime', order: 'descending'}">
+            <el-table-column align="center" width="50" label="序号" type="index"></el-table-column>
+            <el-table-column align="center" width="200px" prop="integralTime" label="获取时间" sortable="custom"></el-table-column>
+            <el-table-column align="center" width="200px" prop="name" label="姓名" sortable="custom"></el-table-column>
+            <el-table-column align="center" width="200px" prop="label" label="项目" sortable="custom"></el-table-column>
+            <el-table-column align="center" width="200px" prop="integralValue" label="积分" sortable="custom"></el-table-column>
+            <el-table-column align="center" width="800px" prop="remark" label="备注"></el-table-column>
+            <el-table-column align="center" width="200" label="操作" fixed="right">
+              <template slot-scope="scope">
+                <el-button type="primary" size="mini" @click="showEditView(scope.row)">编辑</el-button>
+                <el-button type="danger" size="mini" @click="del(scope.row)">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <br />
+          <div style="justify-content:space-between;">
+            <el-pagination background :page-sizes="sizes" :page-size="searchParams.pageSize" @size-change="sizeChange" :current-page="searchParams.pageNum"
+              @current-change="currentChange" layout="sizes, prev, pager, next, jumper, ->, total" :total="total" style="text-align:center"></el-pagination>
+          </div>
+        </div>
+      </el-main>
+    </el-container>
+    <el-dialog :title="dialogTitle" :close-on-click-modal="false" :visible.sync="dialogVisible" :before-close="cancelEdit" width="50%" center>
+      <el-form :model="employeeIntegral" ref="saveForm">
+        <el-row type="flex">
+          <el-col :span="24">
+            <el-form-item label="参与者" label-width="120px">
+              <el-input v-model="employeeIntegral.employeeId" size="mini"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex">
+          <el-col :span="12">
+            <el-form-item label="项目"  label-width="120px">
+              <el-input v-model="employeeIntegral.integralId" size="mini"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="积分"  label-width="120px">
+              <el-input type="number" v-model="employeeIntegral.integralValue" size="mini" :readonly="true"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex">
+          <el-col :span="24">
+            <el-form-item label="时间" label-width="120px">
+              <el-date-picker v-model="employeeIntegral.integralTime" type="datetime" size="mini" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex">
+          <el-col :span="24">
+            <el-form-item label="备注" label-width="120px">
+              <el-input v-model="employeeIntegral.remark" type="textarea" rows="5" size="mini" placeholder="请输入备注"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" size="mini" @click="save('saveForm')">保存</el-button>
+        <el-button size="mini" @click="cancelEdit">取消</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialogTitle: '',
+      dialogVisible: false,
+      employees: '',
+      employeeIntegral: {
+        id: '',
+        employeeId: '',
+        integralId: '',
+        integralTime: '',
+        integralValue: '',
+        remark: '',
+        createUser: '',
+        createTime: '',
+        updateUser: '',
+        updateTime: '',
+        name: '',
+        label: ''
+      },
+      employeeIntegrals: [],
+      integrals: '',
+      integralTime: [],
+      searchParams: {
+        employeeIds: [],
+        integralIds: [],
+        integralEndTime: '',
+        integralStartTime: '',
+        orderBy: '',
+        pageNum: 1,
+        pageSize: 20
+      },
+      sizes: [20, 50, 100],
+      tableLoading: false,
+      total: 1
+    }
+  },
+  methods: {
+    cancelEdit () {
+      this.dialogVisible = false
+      this.emptyData()
+      this.load()
+    },
+    currentChange (currentChange) {
+      this.searchParams.pageNum = currentChange
+      this.load()
+    },
+    del (row) {
+      this.$confirm('此操作将永久删除[' + row.name + ' : ' + row.label + ' : ' + row.integralTime + ']，是否继续？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteRequest('/employee/integral/' + row.id).then(resp => {
+          if (resp && resp.status === 200 && resp.data.success) {
+            this.load()
+          }
+        })
+      }).catch(() => {
+      })
+    },
+    emptyData () {
+      this.employeeIntegral = {
+        id: '',
+        employeeId: '',
+        integralId: '',
+        integralTime: '',
+        integralValue: '',
+        remark: '',
+        createUser: '',
+        createTime: '',
+        updateUser: '',
+        updateTime: '',
+        name: '',
+        label: ''
+      }
+    },
+    integralSearchTimeChange () {
+      var selectTime = this.integralTime
+      if (selectTime && selectTime.length > 0) {
+        this.searchParams.integralStartTime = selectTime[0]
+        this.searchParams.integralEndTime = selectTime[1]
+      }
+      console.log(this.searchParams.integralStartTime)
+      console.log(this.searchParams.integralEndTime)
+    },
+    load () {
+      var _this = this
+      this.tableLoading = true
+      this.getRequest('/employee/integral', _this.searchParams).then(resp => {
+        this.tableLoading = false
+        _this.total = resp.data.total
+        _this.employeeIntegrals = resp.data.data
+      })
+    },
+    save (formName) {
+      var _this = this
+      _this.tableLoading = true
+      this.putRequest('/employee/integral', _this.employeeIntegral).then(resp => {
+        _this.tableLoading = false
+        _this.dialogVisible = false
+        _this.emptyData()
+        _this.load()
+      })
+    },
+    search () {
+      this.searchParams.pageNum = 1
+      this.load()
+    },
+    sizeChange (sizeChange) {
+      this.searchParams.pageSize = sizeChange
+      this.load()
+    },
+    showAddView () {
+      this.dialogTitle = '添加'
+      this.dialogVisible = true
+    },
+    showEditView (row) {
+      var _this = this
+      this.tableLoading = true
+      this.getRequest('/employee/integral/' + row.id).then(resp => {
+        _this.employeeIntegral = resp.data.data
+      })
+      _this.tableLoading = false
+      _this.dialogTitle = '编辑'
+      _this.dialogVisible = true
+    },
+    tableSortChange (column) {
+      if (column.order === 'descending') {
+        this.searchParams.orderBy = column.prop + ' DESC'
+      } else {
+        this.searchParams.orderBy = column.prop + ' ASC'
+      }
+      this.load()
+    }
+  },
+  mounted () {
+    this.load()
+  }
+}
+</script>
+
+<style>
+</style>
