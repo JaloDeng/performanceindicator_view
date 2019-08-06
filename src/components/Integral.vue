396b34ca385f590da3156661b1003d41a1c196e0
JaloDeng <65137928@qq.com>
2019-08-06 22:36:14 +0800

    优化项目积分表拆分

--- src/components/Integral.vue
+++ src/components/Integral.vue
@@ -31,41 +31,12 @@
         </div>
       </el-main>
     </el-container>
-    <!-- <el-dialog :title="dialogTitle" :close-on-click-modal="false" :visible.sync="dialogVisible" :before-close="closeDialog" width="50%" center>
-      <el-form :model="integral" ref="saveForm">
-        <el-row type="flex">
-          <el-col :span="24">
-            <el-form-item label="项目" label-width="120px">
-              <el-input v-model="integral.label" size="mini" placeholder="请输入项目"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row type="flex">
-          <el-col :span="24">
-            <el-form-item label="积分"  label-width="120px">
-              <el-input type="number" v-model="integral.value" size="mini" placeholder="请输入积分"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row type="flex">
-          <el-col :span="24">
-            <el-form-item label="备注" label-width="120px">
-              <el-input v-model="integral.remark" type="textarea" rows="5" size="mini" placeholder="请输入备注"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="primary" size="mini" @click="save('saveForm')">保存</el-button>
-        <el-button size="mini" @click="cancelEdit">取消</el-button>
-      </span>
-    </el-dialog> -->
     <integralEditor :dialogTitle="dialogTitle" :dialogVisible="dialogVisible" :integral="integral" v-on:closeDialog="closeDialog"></integralEditor>
   </div>
 </template>
 
 <script>
-import integralEditor from '@/components/IntegralEditor'
+import integralEditor from '@/components/dialog/IntegralEditor'
 
 export default {
   components: {
