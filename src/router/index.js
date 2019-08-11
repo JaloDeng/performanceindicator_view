4ba47712b3d78b64d141afffac4728c8eea9b2f5
JaloDeng <65137928@qq.com>
2019-08-11 18:26:12 +0800

    优化路由文件

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,6 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: () => import('@/components/Home')
-    },
-    {
-      path: '/viewemployee',
-      name: 'employee',
       component: () => import('@/components/Home'),
       children: [
         {
@@ -21,27 +16,13 @@ export default new Router({
           name: '参与者',
           icon: 'el-icon-user',
           component: () => import('@/components/view/Employee')
-        }
-      ]
-    },
-    {
-      path: '/viewemployeeintegral',
-      name: 'employeeintegral',
-      component: () => import('@/components/Home'),
-      children: [
+        },
         {
           path: '/viewemployeeintegral',
           name: '参与者积分',
           icon: 'el-icon-star-on',
           component: () => import('@/components/view/EmployeeIntegral')
-        }
-      ]
-    },
-    {
-      path: '/viewintegral',
-      name: 'integral',
-      component: () => import('@/components/Home'),
-      children: [
+        },
         {
           path: '/viewintegral',
           name: '项目积分',
